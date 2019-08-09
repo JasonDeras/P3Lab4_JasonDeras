6e91ee66ca62e77b07272425248fac71066d6189
JasonDeras <derasjason2@gmail.com>
2019-08-09 12:59:24 -0600

    Agregando el principal

---
+++ Principal.cpp
@@ -0,0 +1,31 @@
+#include<typeinfo>
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+#include<string.h>
+
+using namespace std;
+
+	int main(){
+		
+		int op;
+		int r=1;
+		
+		while(){
+			cout<<"Menu\n\n\n\n";
+			cout<<"Ingrese una opcion: ";
+			cin>>op;
+			switch(op){
+				
+				default: 
+					cout<<"Opcion no valida\n\n\n\n";
+				break;
+				
+			}//Fin del switch de las opciones
+			cout<<"Volver al menu?1.-Si,2.-No: ";
+			cin>>r;
+			cout<<"\n\n\n\n";
+		}//Fin del while del menu
+		system("pause");
+		return 0;
+	}//Fin del main
